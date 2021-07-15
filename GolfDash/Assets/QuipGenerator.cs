cfcafa450864e34ea87c8d0e430bc22603bd390e
ramewils <ramewils@ucsc.edu>
2021-07-15 14:28:24 -0700

    added quipgenerator

---
+++ GolfDash/Assets/QuipGenerator.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuipGenerator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        3/3: ok legend, tiger 
+    }
+
+    // Update is called once per frame
+   
+}
